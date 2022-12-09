f629d99b4e5cd72c3e2e922286f984cca65de324
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-12-09 17:25:44 +0330

    fixed test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react'
 
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g , ' $1')
+}
+
 function App() {
-  const [buttonColor, setButtonColor] = useState('red')
-  const newButtonColor = buttonColor === "red" ? "blue" : "red"
+  const [buttonColor, setButtonColor] = useState('MediumVioletRed')
+  const [disabled, setDisabled] = useState(false)
+  const newButtonColor = buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed"
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={() => setButtonColor(newButtonColor)}>
-        Change to {newButtonColor}
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}> 
+        Change to {replaceCamelWithSpaces(newButtonColor)}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
